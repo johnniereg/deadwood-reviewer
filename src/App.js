c41c211f53226b2cb147cefc2d96cf49f796b1b6
Johnnie Regalado <johnniereg@gmail.com>
2018-03-02 19:10:27 +0000

    Added normalize and skeleton style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './normalize.css';
+import './skeleton.css';
 import './App.css';
+
 
 class App extends Component {
   render() {
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a class="button" href="#">Anchor button</a>
       </div>
     );
   }
