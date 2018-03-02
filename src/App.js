be5b31f017378d8c946fe4efc2b4673b224e7300
Johnnie Regalado <johnniereg@gmail.com>
2018-03-02 20:45:54 +0000

    Display episodes. Collect votes. Change episodes.
    Render episodes as a component based on the index position of the app. Allow for votes to be clicked and counted in app state. Switch forward and backwards between episodes.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './normalize.css';
 import './skeleton.css';
 import './App.css';
+import Episodes from './episode-details.js';
+import Episode from './components/Episode.js';
+import VoteButton from './components/VoteButton.js';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      likes: 0,
+      dislikes: 0,
+      episodes: Episodes
+    };
+    this.handleVote = this.handleVote.bind(this);
+    this.changeEpisode = this.changeEpisode.bind(this);
+  }
+
+  handleVote(vote) {
+    
+    if (vote === 'like') {
+      console.log("Liked it!");
+      this.setState({
+        likes: this.state.likes + 1
+      });
+    }
+
+    if (vote === 'dislike') {
+      console.log("Disliked it!");
+      this.setState({
+        dislikes: this.state.dislikes + 1
+      });
+    }
+
+  }
+
+  changeEpisode(direction) {
+    if (direction === 'next') {
+      this.setState({
+        index: this.state.index + 1
+      });
+    }
+
+    if (direction === 'previous') {
+      if (this.state.index > 0) {
+        this.setState({
+          index: this.state.index - 1
+        });
+      }
+    }
+
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a class="button" href="#">Anchor button</a>
+        <div className="header container">
+          <h1>Deadwood Reviewer</h1>
+          <h5>Scoreboard:</h5>
+          <h6>Likes: {this.state.likes} </h6>
+          <h6>Dislikes: {this.state.dislikes} </h6>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="one-half column">
+              <Episode index={this.state.index} />
+              <p>Did you like this episode?</p>
+              <div className="row">
+                <div className="one-half column" onClick={(e) => { this.handleVote('like') }}>
+                  <VoteButton buttonType={'like'} buttonName={`I Liked It`} />
+                </div>
+                <div className="one-half column" onClick={(e) => { this.handleVote('dislike') }}>
+                  <VoteButton buttonType={'dislike'} buttonName={`I Didn't Like It`}  />
+                </div>
+              </div>
+            </div>
+            <div className="one-half column">
+              <div onClick={(e) => {this.changeEpisode('next')}}>
+                <p>Next Episode</p>
+              </div>
+              <div onClick={(e) => { this.changeEpisode('previous') }}>
+                <p>Previous Episode</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
