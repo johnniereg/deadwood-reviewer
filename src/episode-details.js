9bdf41b0aa0e797cb0ae213b3ed5247b3c4f891b
Johnnie Regalado <johnniereg@gmail.com>
2018-03-02 20:45:14 +0000

    Array of all episodes

---
+++ src/episode-details.js
@@ -0,0 +1,14 @@
+const Episodes = [
+    { 
+        episode: 1,
+        title: 'Season 1 Episode 1: Deadwood',
+        description: 'In the year 1876, after executing a last act of justice as a Montana marshall, Seth Bullock relocates to a gold mining camp known as Deadwood, where he and partner Sol Star look to start a hardware business. Saloon owner Al Swearengen contends with a bungled robbery turned mass murder that threatens to incite mob violence throughout the camp. Hickok and his companions arrive in Deadwood. The people of Deadwood go off to hunt down the Native Americans who they suspect butchered a family of white people.'
+    },
+    {
+        episode: 2,
+        title: 'Season 1 Episode 2: Deep Water',
+        description: 'As suspicions arise that "road agents" may have been the true perpetrators of the massacre as opposed to Sioux scouts, Swearengen takes a special interest in the health of its sole survivor, a young girl ministered to by the unlikely team of Doc Cochran and Calamity Jane. Brom Garret suspects his newly acquired gold claim may not be all it was advertised.'
+    }
+];
+
+export default Episodes;
