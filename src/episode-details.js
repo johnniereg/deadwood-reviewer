57780ad0b90a378a9fae707615e848228fcc735e
Johnnie Regalado <johnniereg@gmail.com>
2018-03-02 22:34:57 +0000

    Added vote to all episodes

--- src/episode-details.js
+++ src/episode-details.js
@@ -1,116 +1,132 @@
 const Episodes = [
     { 
+        vote: "",
         season: 1,
         episode: 1,
         title: "Deadwood",
-        description: "In the year 1876, after executing a last act of justice as a Montana marshall, Seth Bullock relocates to a gold mining camp known as Deadwood, where he and partner Sol Star look to start a hardware business. Saloon owner Al Swearengen contends with a bungled robbery turned mass murder that threatens to incite mob violence throughout the camp. Hickok and his companions arrive in Deadwood. The people of Deadwood go off to hunt down the Native Americans who they suspect butchered a family of white people.",
-        vote: ''
+        description: "In the year 1876, after executing a last act of justice as a Montana marshall, Seth Bullock relocates to a gold mining camp known as Deadwood, where he and partner Sol Star look to start a hardware business. Saloon owner Al Swearengen contends with a bungled robbery turned mass murder that threatens to incite mob violence throughout the camp. Hickok and his companions arrive in Deadwood. The people of Deadwood go off to hunt down the Native Americans who they suspect butchered a family of white people."
     },
     {
+        vote: "",
         season: 1,        
         episode: 2,
         title: "Deep Water",
-        description: "As suspicions arise that 'road agents' may have been the true perpetrators of the massacre as opposed to Sioux scouts, Swearengen takes a special interest in the health of its sole survivor, a young girl ministered to by the unlikely team of Doc Cochran and Calamity Jane. Brom Garret suspects his newly acquired gold claim may not be all it was advertised.",
-        vote: ''
+        description: "As suspicions arise that 'road agents' may have been the true perpetrators of the massacre as opposed to Sioux scouts, Swearengen takes a special interest in the health of its sole survivor, a young girl ministered to by the unlikely team of Doc Cochran and Calamity Jane. Brom Garret suspects his newly acquired gold claim may not be all it was advertised."
     },
     {
+        vote: "",
         season: 1,
         episode: 3,
         title: "Reconnoitering the Rim",
-        description: "Competition arrives for Swearengen in the form of the Bella Union, a new gambling outfit from Chicago operated by savvy Cy Tolliver, Madame Joanie Stubbs and gambling guru Eddie Sawyer. Hickok puts up precious collateral in a poker game with McCall, and a threat from Brom Garret regarding his gold claim invites harsh consequences. The relationship between Bullock and Swearengen continues to worsen over the latter's suspicions of Bullock's and Hickok's intentions.",
-        vote: ''
+        description: "Competition arrives for Swearengen in the form of the Bella Union, a new gambling outfit from Chicago operated by savvy Cy Tolliver, Madame Joanie Stubbs and gambling guru Eddie Sawyer. Hickok puts up precious collateral in a poker game with McCall, and a threat from Brom Garret regarding his gold claim invites harsh consequences. The relationship between Bullock and Swearengen continues to worsen over the latter's suspicions of Bullock's and Hickok's intentions."
     },
     {
+        vote: "",
         season: 1,
         episode: 4,
         title: "Here Was a Man",
-        description: "Swearengen directs Farnum to buy back the Garret claim after Dority discovers that the land actually does contain gold; Alma Garret prevails on Calamity Jane and Hickok for help regarding her late husband's suspicious death. Hickok commissions Bullock in his stead as McCall's growing anger toward Hickok leads to a tragic murder of the latter."
+        description: "Swearengen directs Farnum to buy back the Garret claim after Dority discovers that the land actually does contain gold; Alma Garret prevails on Calamity Jane and Hickok for help regarding her late husband's suspicious death. Hickok commissions Bullock in his stead as McCall's growing anger toward Hickok leads to a tragic murder of the latter.",
     },
     {
+        vote: "",
         season: 1,
         episode: 5,
         title: "The Trial of Jack McCall",
         description: "Swearengen transforms The Gem into a courtroom as Deadwood makes its own laws to try Jack McCall. With Jane off on a bender, Trixie is enlisted by Swearengen to help Alma with the Metz child and to keep her pliable to his purposes for getting back the Garret claim. Hickok's body is buried and Seth Bullock is determined to get revenge for his friend's murder."
     },
     {
+        vote: "",
         season: 1,
         episode: 6,
         title: "Plague",
         description: "Bullock encounters native resistance in his quest to bring a murderer to justice. Swearengen presses a resentful Farnum to keep tabs on Alma and Trixie as the camp fathers pool their resources to dispatch riders after the precious vaccine and build a sick tent as smallpox arrives in Deadwood. Cochran turns to a distressed Jane for help caring for the diseased in the coming days."
     },
     {
+        vote: "",
         season: 1,
         episode: 7,
         title: "Bullock Returns to the Camp",
         description: "After tracking down McCall, Bullock returns to Deadwood a changed man – and a marked one – while Utter pays final respects to a fallen friend. Trixie feels uneasy about her charge, which prompts a violent response from Swearengen. Cy Tolliver is shocked, and displeased, to see Andy Cramed return to his saloon alive. Miles and Flora, two young siblings arrive at Deadwood looking for their missing father and work."
     },
     {
+        vote: "",
         season: 1,
         episode: 8,
         title: "Suffer the Little Children",
         description: "Deadwood breathes easier when riders arrive with precious vaccine – and word of a possible treaty with the Sioux. Awaiting Trixie’s return, a pragmatic Swearengen resists counsel from a bloody-minded Farnum. Tolliver teaches a cruel lesson to two would-be thieves; and to his madame."
     },
     {
+        vote: "",
         season: 1,
         episode: 9,
         title: "No Other Sons or Daughters",
         description: "With annexation looming, Swearengen calls a meeting to set up an informal government. Bullock and Alma compare notes on Ellsworth, and each other. Joanie searches for new options in Deadwood as Jane heads out of town to find hers. The health of the camp's Reverend continues to worsen, a plight that begins to weigh heavy on the mind of Doctor Cochran."
     },
     {
+        vote: "",
         season: 1,
         episode: 10,
         title: "Mister Wu",
         description: "An opium theft leaves Swearengen trying to find common language with his supplier and the boss of the Deadwood Chinese population, Mr. Wu, as well as navigating tricky waters to deal with the mess. A letter from a Yankton magistrate infuriates Swearengen, but finds the solution may well lie with the letter's deliverer. Eddie Sawyer promises to back Joanie for her brothel."
     },
     {
+        vote: "",
         season: 1,
         episode: 11,
         title: "Jewel's Boot Is Made for Walking",
         description: "Alma’s father, Otis Russell, arrives from New York City to “help” with her claim but soon begins to manipulate his daughter. Swearengen strikes a bargain with Adams to get rid of the Yankton magistrate. Trixie and Sol have sex at the hardware store, much to the ire of Swearengen as Bullock expresses his distaste at the settlement's new sheriff."
     },
     {
+        vote: "",
         season: 1,
         episode: 12,
         title: "Sold Under Sin",
         description: "The 7th Cavalry under General George Crook, a.k.a. “Custer's avengers,” rolls into Deadwood, prompting a parade – and business solicitations from Farnum and Tolliver. Swearengen finally deals with the Yankton magistrate and gains a new ally in Silas Adams. Bullock confronts Otis Russell over his intentions as his relationship with Alma deepens. Doc Cochran's prayers are answered and Reverend Smith is put out of his misery. Bullock becomes sheriff of Deadwood."
     },
     {
+        vote: "",
         season: 2,
         episode: 1,
         title: "A Lie Agreed Upon (Part I)",
         description: "Anxious about Deadwood’s future, Swearengen calls out Bullock over the Sheriff's relationship with Alma; the resulting confrontation turns inadvertently public—and violent. Bullock's wife and step son arrive as do Joanie's new business partners."
     },
     {
+        vote: "",
         season: 2,
         episode: 2,
         title: "A Lie Agreed Upon (Part II)",
         description: "As the day’s injured recoup, the night brings a new set of tensions. Alma and an increasingly self-destructive Bullock face an abrupt decision on their future, even as the Sheriff seeks to make good a family pledge to recover his badge and guns from Swearengen, with help from Utter and a newly returned Jane."
     },
     {
+        vote: "",
         season: 2,
         episode: 3,
         title: "New Money",
         description: "Francis Wolcott, chief geologist for a powerful mining operation, arrives to shake up the status quo – beginning with Tolliver. Dority, Burns and Trixie fret as an ailing Swearengen refuses visitors and medical attention. Farnum's attempts at blackmail backfire, leaving him indebted to the new arrival. The mysterious benefactor of the Chez Amis brothel makes himself known, much to Joanie's unease."
     },
     {
+        vote: "",
         season: 2,
         episode: 4,
         title: "Requiem for a Gleet",
         description: "Cochran contemplates a procedure that could cure Swearengen or kill him. His employer indisposed, Dority must attend to a former fellow-bushwacker come to request Swearengen’s permission to pull a job, and to Wu, who attempts to convey news of a “tong” newly arrived from San Francisco who appears to be an associate of Francis Wolcott. Sol Star ponders plans for setting up a bank, but his choice of possible backers displeases Bullock. Ellsworth confronts Wolcott on the Garret claim."
     },
     {
+        vote: "",
         season: 2,
         episode: 5,
         title: "Complications",
         description: "As a convalescing Swearengen bridles, Cochran schools Burns and Dority how best to assist his recovery. Alma too is feeling unwell in the mornings which leads her to believe she is pregnant, making her look for Trixie and Doc's counselling. Unexpected profits at the 'Chez Amis' lead Tolliver to a startling discovery about Wolcott. Concerns over the safety of gold claims once Deadwood is annexed leads to a panic amongst prospectors that turns violent – and when the local commissioner is whisked to safety by Bullock, an old friend of Hostetler becomes the target of their ire."
     },
     {
+        vote: "",
         season: 2,
         episode: 6,
         title: "Something Very Expensive",
         description: "Alma proposes the formation of the Deadwood bank, and looks to familiar faces to run it. To test himself, Swearengen grants audience to a long line of supplicants; talks with Farnum and Miss Isringhausen, Alma's teacher for Sofia Metz, prove most illuminating. Tolliver confronts Wolcott regarding personal details he has uncovered, which sends the latter into a murderous rage."
     },
     {
+        vote: "",
         season: 2,
         episode: 7,
         title: "E.B. Was Left Out",
@@ -123,96 +139,112 @@ const Episodes = [
         description: "Wolcott offers on a rich claim operated by two ornery brothers, and reports to his boss on their progress overall. Tom Nuttall proudly unveils his new 'Bone-Shaker' bicycle—but some doubt he can ride it. Swearengen and Bullock discuss alternatives to annexing the Hills to Dakota."
     },
     {
+        vote: "",
         season: 2,
         episode: 9,
         title: "Amalgamation and Capital",
         description: "Merrick goes overboard printing rumors in 'The Deadwood Pioneer,' earning rebuke from Swearengen. Tolliver aims to fleece Mose Manuel of his bloodstained profits. Utter's enraged when Wolcott interrupts his and Bullock's interrogation of Mose, and decides to leave town; the unsent letter of an old friend to his wife bringing back painful memories. A friendship between Jane and Joanie begins to grow as the latter becomes more isolated and alone. An escaped horse violently knocks William Bullock down in the thoroughfare."
     },
     {
+        vote: "",
         season: 2,
         episode: 10,
         title: "Advances, None Miraculous",
         description: "In the wake of a tragic accident, Cochran delivers a dire prognosis as the entire camp stands vigil. Swearengen enlists Star and Adams to help improvise a con on the newly-returned Commissioner Jarry as he deals with the Pinkerton agent. Andy Cramed, former Deadwood pariah, offers himself as the camp's new minister. Martha regrets her move to Deadwood."
     },
     {
+        vote: "",
         season: 2,
         episode: 11,
         title: "The Whores Can Come",
         description: "Telegraph lines to Yankton heat up as Jarry fears Bullock and Hearst have each thrown their weight behind Montana as their ruse begins to fall into place. Swearengen uses a camp tragedy to delay an altercation between Wu and Lee. Ellsworth and Alma come to an agreement."
     },
     {
+        vote: "",
         season: 2,
         episode: 12,
         title: "Boy-the-Earth-Talks-To",
         description: "As Deadwood readies for a celebration, George Hearst's arrival in camp brings upheaval. Swearengen's manipulations extract a counter-offer from Yankton. Hearst comes to separate arrangements with Farnum and Swearengen as Wolcott faces the consequences of his actions. The conflict amongst the Chinese bosses comes to a bloody climax."
     },
     {
+        vote: "",
         season: 3,
         episode: 1,
         title: "Tell Your Good to Ready for Blood",
         description: "Elections for mayor and sheriff are fast approaching, but Bullock and Swearengen keep an eye on influential new resident George Hearst, who tests the waters of the camp's power structure as union organizers begin turning up murdered on Swearengen's property. Adams proposes a real-estate deal to Star at the behest of Swearengen; Alma encounters complications with her pregnancy."
     },
     {
+        vote: "",
         season: 3,
         episode: 2,
         title: "I Am Not the Fine Man You Take Me For",
         description: "The candidates for sheriff and mayor deliver their campaign speeches. As Swearengen refuses to genuflect, Hearst gives him a personal demonstration of his influence and power. Doc Cochran tends to an ailing Alma as Ellsworth's fears grow. Jane shares her exploits with Custer with the schoolchildren at Martha's behest. Andy has an uncomfortable reunion with Tolliver."
     },
     {
+        vote: "",
         season: 3,
         episode: 3,
         title: "True Colors",
         description: "The stagecoach brings Hearst's loyal cook Aunt Lou, theater promoter Jack Langrishe (an old friend of Swearengen's), and a returning (and much Westernized) Mr. Wu. Hearst shows he means business during separate dealings with Alma, Bullock, and Tolliver as more unionizing miners are murdered by agents of Hearst."
     },
     {
+        vote: "",
         season: 3,
         episode: 4,
         title: "Full Faith and Credit",
         description: "Alma opens Deadwood's first bank. Hearst meets with Swearengen and Tolliver and lays out his plans to consolidate his power. As he and Martha deal with painful memories of William's death, Bullock brokers a deal between Steve and Hostetler when Hostetler and Samuel Fields arrive back at the camp."
     },
     {
+        vote: "",
         season: 3,
         episode: 5,
         title: "A Two-Headed Beast",
         description: "Dority picks up Captain Turner's gauntlet and the two do battle in the thoroughfare. Hostetler completes a deal regarding the livery, but a final insult against his honor pushes him over the edge. Bullock hands Hearst an earful and makes a public arrest as the latter mourns a blow to his operations."
     },
     {
+        vote: "",
         season: 3,
         episode: 6,
         title: "A Rich Find",
         description: "Bullock and Swearengen contemplate a preemptive strike against Hearst. Aunt Lou reunites with her son, Odell, who brings a business proposal to his mother's employer. Trixie confronts Alma regarding her returning drug habit and Ellsworth moving out of their home, as Leon informs Tolliver of the situation regarding Alma. An ailing Jane moves in with Joanie."
     },
     {
+        vote: "",
         season: 3,
         episode: 7,
         title: "Unauthorized Cinnamon",
         description: "Camp elders meet at the Gem to discuss the Hearst situation. Bullock offers a solution to the standoff. Swearengen learns of Doc Cochran's illness. Hearst strikes a deal with Odell, and admits his growing hatred of Deadwood and its inhabitants."
     },
     {
+        vote: "",
         season: 3,
         episode: 8,
         title: "Leviathan Smiles",
         description: "Wyatt Earp and his brother arrive in Deadwood, raising suspicions as to whom they work for. The town awaits Hearst's next move as additional reinforcements arrive to help Hearst maintain his power. Aunt Lou's concerns over Odell's safety grow in the wake of his planned involvement with Hearst. Tolliver and Hearst plot against both the Earps and Bullock. Langrishe mourns the passing of an old friend."
     },
     {
+        vote: "",
         season: 3,
         episode: 9,
         title: "Amateur Night",
         description: "Langrishe welcomes all comers for the camp's first amateur night as his theater finally opens. A returning Commissioner Jarry seeks an audience with Hearst regarding the coming elections as Hearst orders violence against Merrick. Aunt Lou receives distressing news about her son."
     },
     {
+        vote: "",
         season: 3,
         episode: 10,
         title: "A Constant Throb",
         description: "With Bullock out of camp and Ellsworth on the Garret claim, Alma survives a close call involving Hearsts's hired guns – only to receive help from an unlikely source. Swearengen responds to a message from Hearst by killing his messenger. The relationship between Joanie and Jane deepens."
     },
     {
+        vote: "",
         season: 3,
         episode: 11,
         title: "The Catbird Seat",
         description: "Hearst follows one close call with a more on-target one, forcing Bullock to cut short a campaign speech in Sturgis and uniting Deadwood in its grief for the death of one of their own. Trixie attempts to exact revenge. Swearengen, Wu, and Adams plan their retaliation. Tolliver becomes more and more uneasy with his pariah status."
     },
     {
+        vote: "",
         season: 3,
         episode: 12,
         title: "Tell Him Something Pretty",
